9da8ac0cec9b298d502b7abdecd5b5ce2e7908de
Luis Fernando Brasil <luis4756@live.com>
2024-07-15 20:50:55 -0300

    Ajustes Listas

--- cadastros/views.py
+++ cadastros/views.py
@@ -1,30 +1,85 @@
 from .models import Cidade, Pessoa
 
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import DeleteView
+from django.views.generic.list import ListView
+
 from django.urls import reverse_lazy
 
+
 class CidadeCreate(CreateView):
+    template_name = 'form.html'
+    success_url = reverse_lazy('listar-cidade')
     model = Cidade
     fields = ['nome', 'estado']
-    template_name = 'form.html'
-    success_url = reverse_lazy('index')
-    
-class PessoaCreate(CreateView):
-    model = Pessoa
-    fields = ['nome_completo', 'nascimento', 'cpf', 'email',
-              'rede_social', 'salario', 'cidade']
-    template_name = 'form.html'
-    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        dados = super().get_context_data(**kwargs)
+        dados['titulo'] = 'Cadastrar cidade'
+        return dados
+
 
 class CidadeUpdate(UpdateView):
+    template_name = 'form.html'
+    success_url = reverse_lazy('listar-cidade')
     model = Cidade
     fields = ['nome', 'estado']
+
+    def get_context_data(self, **kwargs):
+        dados = super().get_context_data(**kwargs)
+        dados['titulo'] = 'Editar registro de Cidade'
+        return dados
+
+
+class CidadeDelete(DeleteView):
+    template_name = 'form-excluir.html'
+    success_url = reverse_lazy('listar-cidade')
+    model = Cidade
+
+
+class CidadeList(ListView):
+    template_name = 'list/cidade.html'
+    model = Cidade
+
+##############################################################
+
+
+class PessoaCreate(CreateView):
     template_name = 'form.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-pessoa')
+    model = Pessoa
+    fields = [
+        'nome_completo', 'nascimento', 'cpf', 'email',
+        'rede_social', 'salario', 'cidade',
+    ]
+
+    def get_context_data(self, **kwargs):
+        dados = super().get_context_data(**kwargs)
+        dados['titulo'] = 'Cadastrar nova Pessoa'
+        return dados
+
 
 class PessoaUpdate(UpdateView):
-    model = Pessoa
-    fields = ['nome_completo', 'nascimento', 'email',
-              'rede_social', 'salario', 'cidade']
     template_name = 'form.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-pessoa')
+    model = Pessoa
+    fields = [
+        'nome_completo', 'nascimento', 'cpf', 'email',
+        'rede_social', 'salario', 'cidade',
+    ]
+
+    def get_context_data(self, **kwargs):
+        dados = super().get_context_data(**kwargs)
+        dados['titulo'] = f"Editar registro de {self.object.nome_completo}"
+        return dados
+
+
+class PessoaDelete(DeleteView):
+    template_name = 'form-excluir.html'
+    success_url = reverse_lazy('listar-pessoa')
+    model = Pessoa
+
+
+class PessoaList(ListView):
+    template_name = 'list/pessoa.html'
+    model = Pessoa
